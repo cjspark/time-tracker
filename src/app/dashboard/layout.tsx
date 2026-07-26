364d8b4e0927518644588873b2744e2f38b9b301
Juan Cao <juan.cao@sap.com>
2026-07-26 15:49:03 -0400

    fix: respect iOS safe area inset top

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -31,7 +31,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 
   return (
     <TimerProvider>
-      <div className="fixed inset-0 flex flex-col bg-gray-50">
+      <div className="fixed inset-0 flex flex-col bg-gray-50" style={{ paddingTop: 'env(safe-area-inset-top)' }}>
         {/* Fixed header */}
         <div className="shrink-0 z-40 bg-white">
           <AppHeader />
